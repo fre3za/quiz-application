f9360e7102303779044b3c850f16990325d20cdf
yash paul <143523461+fre3za@users.noreply.github.com>
2025-03-18 15:04:48 +0530

    constants

---
+++ commonConstants.java
@@ -0,0 +1,10 @@
+package quizapplication.src.constants;
+import java.awt.*;
+public class commonConstants {
+//colors 
+public static final Color LIGHT_BLUE = Color.decode("#5699FD");
+public static final Color DARK_BLUE = Color.decode("#5699E");
+public static final Color BRIGHT_YELLOW = Color.decode("#B7FF5B");
+public static final Color LIGHT_RED = Color.decode("#FF8585");
+public static final Color LIGHT_GREEN = Color.decode("#76F592");
+}
